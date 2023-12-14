30cc82fac98f7e4ce748c8a163d15f3b2a91081a
Anish <simplifypib@gmail.com>
2023-12-14 13:02:02 +0530

    initial commit

---
+++ backend/src/test/java/com/amit/smo/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.smo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Spotify Music Organizer</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
